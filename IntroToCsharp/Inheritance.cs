17f211528290e9bd259b4a41be9ea8c516a094ea
Rahulburugula <155060337+Rahulburugula@users.noreply.github.com>
2024-01-11 20:24:17 +0530

    Add files via upload

---
+++ IntroToCsharp/Inheritance.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+// sealed keyword is used before class keyword to avoid inheritance from that class
+// e.g. public sealed class Employee {}
+
+//Mutiple Inheritance is not supported , 
+//    But Multiple Interface and multi-level Inheritance is supported
+
+namespace IntroToCsharp
+{
+    public class Employee
+    {
+        public string FirstName, LastName, Email;
+        public void PrintFullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        public float YearlySalary;
+    }
+
+    public class PartTimeEmployee : Employee
+    {
+        public float HourlyRate;
+    }
+    internal class Inheritance
+    {
+        public void func()
+        {
+            FullTimeEmployee FTE = new FullTimeEmployee();
+            FTE.FirstName = "Rahul";
+            FTE.LastName = "Barnwal";
+            FTE.YearlySalary = 10000000;
+            FTE.PrintFullName();
+
+            PartTimeEmployee PTE = new PartTimeEmployee();
+            PTE.FirstName = "Pragim";
+            PTE.LastName = "Tech";
+            PTE.HourlyRate = 5000000;
+            PTE.PrintFullName();
+        }
+    }
+}
---
+++ IntroToCsharp/Program.cs
@@ -0,0 +1,86 @@
+﻿using IntroToCsharp;
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        //ReadingAndWriting readingAndWriting = new ReadingAndWriting();
+        //readingAndWriting.func();
+
+        //StringType stringType = new StringType();
+        //stringType.func();
+
+        //TypeConversion typeConversion = new TypeConversion();
+        //typeConversion.func();
+
+        //IntroToClasses introToClasses = new IntroToClasses();
+        //introToClasses.SomeFunction();
+
+        //StaticAndInstance staticAndInstance = new StaticAndInstance();
+        //staticAndInstance.func();
+
+        //Inheritance inheritance = new Inheritance();
+        //inheritance.func();
+
+        //Assignment_1 assignment1 = new Assignment_1();
+        //assignment1.Func();
+
+        //MethodHiding methodHiding = new MethodHiding();
+        //methodHiding.Func();
+
+        //Polymorphism polymorphism = new Polymorphism();
+        //polymorphism.Func();
+
+        //OverridingVsHiding overridingVsHiding = new OverridingVsHiding();
+        //overridingVsHiding.Func();
+
+        //MethodOverloading.Func();
+
+        //Structs structs = new Structs();
+        //structs.Func();
+
+        //Interfaces interfaces = new Interfaces();
+        //interfaces.Func();
+
+        //ExplicitInterfacesImplementation.Func();
+
+        //AbstractClasses.Func();
+
+        //MultipleClassInheritanceUsingInterfaces.Func();
+
+        //DelegatesUsage.Func();
+
+        //MulticastDelegates.Func();
+
+        //ExceptionalHandling.Func();
+
+        //InnerException.Func();
+
+        //CustomException.Func();
+
+        //ExceptionHandlingAbuse.Func();
+
+        //PreventingExceptionHandlingAbuse.Func();
+
+        //WhyEnums.Func();
+
+        //EnumsExample.Func();
+
+        //Enums.Func();
+
+        //Attributes.Func();
+
+        //Reflection.Func();
+
+        //LateBinding.Func();
+
+        //Generics.Func();
+
+        EqualsOverriding.Func();
+
+        //ConvertToStringVSToString.Func();
+
+        //StringVSStringBuilder.Func();
+    }
+}
